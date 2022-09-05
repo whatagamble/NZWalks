6779a06d6618008281085988c5153682f06fd49d
whatagamble <steven.gamble@outlook.com>
2022-09-05 13:19:10 +0100

    End of Section Two

---
+++ NZWalks/NZWalks.API/Data/NZWalksDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDBContext : DbContext
+    {
+        public NZWalksDBContext(DbContextOptions<NZWalksDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+
+    }
+}
